72ab47da4c6b3bafdcee9e0afca57881ce0d9f58
Casey Provost <casey.provost@spreemo.com>
2019-05-12 12:58:55 -0400

    switch test framework to spec and remove port from production host settings

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,10 @@ module MasterAuth
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.generators do |g|
+      g.test_framework :rspec
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
